0cea781b2f5da0f6901d13e3422b362f23bdda8e
Roger Chappel <miscanalysis@gmail.com>
2026-05-17 18:37:48 +1000

    feat: expose library API

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+export { analyzePatch } from "./analyze.js";
+export { hasErrors } from "./policy.js";
+export { renderJson, renderMarkdown } from "./render.js";
+export type {
+  AnalyzeOptions,
+  CliOptions,
+  CommitEvidence,
+  FileChange,
+  GitCommit,
+  PatchLedger,
+  PolicyIssue,
+} from "./types.js";
